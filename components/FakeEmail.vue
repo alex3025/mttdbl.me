e451393e768529bbd3f69c9fed912fe9bb703176
alex3025 <alex3025game@gmail.com>
2024-06-13 19:28:53 +0200

    Made the email clickable and "clear" but only when rendered by Vue

--- components/FakeEmail.vue
+++ components/FakeEmail.vue
@@ -1,4 +1,12 @@
 <template>
   <!-- prettier-ignore -->
-  <span class="font-(mono !medium) dark:text-white text-black whitespace-nowrap">hello<Icon name="ion:ios-at" class="w-4.5 h-4.5" />mttdbl.me</span>
+  <span class="font-(mono !medium) dark:text-white text-black whitespace-nowrap">
+    <ClientOnly fallbackTag="span">
+      <NuxtLink to="mailto:hello@mttdbl.me">hello@mttdbl.me</NuxtLink>
+
+      <template #fallback>
+        hello<Icon name="ion:ios-at" class="w-4.5 h-4.5" />mttdbl.me
+      </template>
+    </ClientOnly>
+  </span>
 </template>
